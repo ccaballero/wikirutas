d906f2df6bcb8d7c8cda61c66e42096ad21c1ded
Carlos Eduardo Caballero Burgoa <cijkb.j@gmail.com>
2014-11-16 06:21:58 -0400

    working on geojson

--- public/js/app.js
+++ public/js/app.js
@@ -2,13 +2,11 @@ var map=new L.Map('map').setView([-17.36651,-66.17563],15);
 
 L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
     maxZoom:18
-  , attribution:'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
-        '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-        'Imagery © <a href="http://mapbox.com">Mapbox</a>'
   , id:'examples.map-20v6611k'
 }).addTo(map);
 
 var layer1=new L.GeoJSON.AJAX('/geojson/k.json').addTo(map);
+var layer2=new L.GeoJSON.AJAX('/geojson/3.json').addTo(map);
 
 //L.marker([51.5, -0.09])
 // .addTo(map)
